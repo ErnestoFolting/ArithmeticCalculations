16688536a7d26ef8b4d9676d78edea6fb31f9c17
Oleksandr <OlexandrTanasienko@gmail.com>
2021-03-20 19:23:31 +0200

    Write private members, implemented size() and empty() for stack
    Add empty function bodies

--- ArithmeticCalculations/stack.h
+++ ArithmeticCalculations/stack.h
@@ -3,6 +3,9 @@
 template <typename T>
 class stack
 {
+private:
+	int itsSize;
+	int itsCapacity;
 public:
 	void push(T data);
 	void pop();
@@ -11,3 +14,32 @@ public:
 	bool empty();
 };
 
+template <typename T>
+void stack<T>::push(T data)
+{
+	
+}
+
+template <typename T>
+void stack<T>::pop()
+{
+	
+}
+
+template <typename T>
+const T & stack<T>::top()
+{
+	
+}
+
+template <typename T>
+int stack<T>::size()
+{
+	return itsSize;
+}
+
+template <typename T>
+bool stack<T>::empty()
+{
+	return (size>0)?true:false;
+}
