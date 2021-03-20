c303f8d24f62224a292f73e3744191a7fb3e03fa
Oleksandr <OlexandrTanasienko@gmail.com>
2021-03-20 20:01:21 +0200

    Implement push() method for stack

--- ArithmeticCalculations/stack.h
+++ ArithmeticCalculations/stack.h
@@ -5,7 +5,10 @@ class stack
 {
 private:
 	int itsSize;
+	const int addedCapacity;
 	int itsCapacity;
+	int * itsData;
+	int itsCurrent; 
 public:
 	stack();
 	stack(int capacity);
@@ -18,20 +21,40 @@ public:
 
 template <typename T>
 stack<T>::stack():
-itsSize(0), itsCapacity(20)
+itsSize(0), addedCapacity(20), itsCapacity(addedCapacity),  itsData(new T[itsCapacity]), itsCurrent(0)
 {}
 
 template <typename T>
 stack<T>::stack(int capacity):
-itsSize(0), itsCapacity(capacity)
+itsSize(0), addedCapacity(capacity), itsCapacity(addedCapacity), itsData(new T[itsCapacity]), itsCurrent(0)
 {}
-
-
 
 template <typename T>
 void stack<T>::push(T data)
 {
-	
+	if(size==0)
+	{
+		itsData[0]=data;
+	}
+	else if(itsSize!=itsCapacity)
+	{
+		itsCurrent++;
+		itsData[itsCurrent]=data;
+	}
+	else if(itsSize==itsCapacity)
+	{
+		itsCapacity+=addedCapacity;
+		int * newData=new T[itsCapacity];
+		for(int i=0;i<itsCurrent;i++)
+		{
+			newData[i]=itsData[i];
+		}
+		delete itsData;
+		itsData=newData;
+		itsCurrent++;
+		itsData[itsCurrent]=data;
+	}
+	itsSize++;
 }
 
 template <typename T>
